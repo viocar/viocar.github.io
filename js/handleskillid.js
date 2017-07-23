8a92e90ab20d44195d93f1db3f370e6ebc499928
viocar <thatkles@gmail.com>
2017-07-23 17:05:54 -0400

    are you really making me do this

---
+++ js/handleskillid.js
@@ -0,0 +1,31 @@
+skill_id.onchange = function(){
+	var s_id = parseInt(skill_id.value);
+	var skillTable = "/tbl/playerskilltable.tbl";
+	if (document.getElementById("enemybox").checked){
+		skillTable ="/tbl/enemyskilltable.tbl";
+	}
+	if (!isNaN(s_id)){
+		var skillFileData = new XMLHttpRequest();
+		skillFileData.open("GET", skillTable, true);
+		skillFileData.responseType = "blob";
+		debugText(skillTable);
+		debugText(skillFileData.response);
+		skillFileData.onload = function(oEvent){
+			var sdBuffer = skillFileData.response;
+			if (sdBuffer){
+				drawSkillTable();
+			} else {
+				console.log("it's fucked");
+			}
+		}
+	} else {
+		console.log("it's nan buddy. better get you a number");
+	}
+}
+
+function debugText(text){
+	var onoff = 1;
+	if (onoff == 1){
+		console.log(text);
+	}
+}
