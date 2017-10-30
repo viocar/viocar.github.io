3cc1c933874d53daac8d296d92b481df8cc041da
viocar <thatkles@gmail.com>
2017-10-30 10:19:41 -0400

    ...

--- js/monsterobj.js
+++ js/monsterobj.js
@@ -8,10 +8,12 @@ const conditionalobj = {
 	"4096":"Kill with ice",
 	"4608":"Kill with volt",
 	"5632":"Kill with poison dmg.",
+	"6144":"Kill while paralyzed",
 	"6656":"Kill while blinded",
+	"7168":"Kill while panicked",
 	"8192":"Kill with curse dmg.",
 	"8704":"Kill while sleeping",
-	"9728":"Kill by petrify",
+	"9728":"Kill while petrified",
 	"10240":"Kill by instant death",
 	"10752":"Kill while head bound",
 	"11264":"Kill while arm bound",
@@ -22,7 +24,10 @@ const conditionalobj = {
 	"13825":"Kill with solo party",
 	"15360":"Don't get rare drop",
 	"17558":"Kill w/ mush. dagger",
-	"18482":"Kill w/ 5,000 dmg. atk."
+	"18482":"Kill w/ 5,000 dmg. atk.", //below this one may not be in EO3
+	"20480":"Kill with any phys",
+	"20992":"Kill with any elem",
+	"21504":"Kill while any bound",
 }
 
 const ename = ["Blank","Claw Shrimp","Fanged Fish","Devilfish","Starry Slug","Aplysiomorpha","Bolt Striker","Venomshroom","Trip Mushroom","Stun Eryngii","Cursed Shroom","Whorled Puffer","Red Starfish","Blue Starfish","King Starfish","Dragon Egg","Pandora Egg","Dragon Pup","Otherspawn","Pasaran","Clionean","Ringed Lemur","Claw Monkey","Great Platypus","Deadly Durian","Rotten Fruit","Nautilus","Ammonite","Hungry Coral","Rock Coral","Lava Beast","Deep Medium","Deep Maiden","Mistletoe","Vampire Tree","Insectortoise","Gigapede","Great Lynx","Flame Lynx","Volt Cat","High Onnep","High Walrus","Freed Savage","Coral Octopus","Silver Octopus","Deep Marine","Deep Raider","Ipupiara","White Fox","Divine Judge","Longicorn","Titan Arum","Miasma Bud","Great Anaconda","Silverback","Sea Wanderer","Big Snakehead","Cruel Roamer","Koolasuchus","Omnivore","Great Anteater","Deep Prelate","Deep Coistrel","Somnovore","Dreamsmasher","Largebill","Raging Tyrant","Greedy Lizard","Poison Dragon","Scaled Dragon","Mother Dragon","Stegosaurus","Great Hermit","Flame Lizard","Rattle Nozuchi","Guardian Ant","Killer Ant","Ptarmigan","Treerat","Forest Hare","Clawed Mole","Swordfish","Dementia Owl","Eerie Statue","Petaloid","Venomfly","Death Pumpkin","Hell Pumpkin","Tri-Pumpkin","Suicide Hare","Forest Frog","Spotted Frog","Furyhorn","Cursed Knight","Raging Boar","Death Scorpion","Sickwood","Dragonfly","Scylla Crab","Abyssal Death","Wicked Queen","Mud Lurker","Corotrangul","Iron Turtle","Cave Elephant","Sauroposeidon","Mortal Hunter","Revelationer","Pasaran","Pasaran","Pasaran","Gold Pasaran","Red Pasaran","Blue Pasaran","Sea Lobster","Forest Shrimp","Red Puffer","Yawnypus","Demon Octopus","Golden Idol","Silver Fox","Peach Crow","Longicorn Beta","Bog Lurker","Tentacle","Penguin","Deep Believer","Deep Sentinel","Sea Berserker","Calamity God","Glutton Beast","Pomona Taker","Pomona Taker","Attendant","Pincer Statue","Roaming Spirit","Splitstool","Avian King","Majordomo","Peach Crow","Largebill","Deep Lady","Deep One","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","Drake","Dragon","Wyrm","Golem","Manticore","Scylla","Alraune","NONE","NONE","NONE","Narmer","Ketos","Gatekeeper","Keeper Head","Keeper Body","Automaton King","Olympia","Deep Princess","Kujura","Progenitor","Abyssal God","Abyssal God","Hammerhead","Ghost Ship","Meregho Saeno","Leviathan","King Penguin","Anemone","Ruin Caller","Frost Head","Flame Head","Thunder Head","Elder Dragon","Shin","Kraken","Kirin","Abyssal God","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE","NONE"];
