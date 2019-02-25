8759275ceb5631fc7216c208b20251f0046fb599
viocar <thatkles@gmail.com>
2019-02-25 14:37:19 -0500

    update...

--- js/n/skillobj.js
+++ js/n/skillobj.js
@@ -7,12 +7,22 @@ const subheader_obj = {
     0x6:"Effect", //seems to be a generic subheader... it's on upper stance but also the survivalist item thing
     0xC:"Maximum activations", //Scapegoat
     0xE:"Maximum activations", //Chase Heal (is this per person?)
+    0x2E:"Lifesteal", //Blood Edge
+    0x4E:"TP Lifesteal", //Gae Bolg
+    0x53:"Splash percent", //Splash Shot
     0x62:"Target type modifier",
+    0x69:"Crit while Upper St.", //Upward Slash
+    0x6A:"Crit while Clear St.", //Charging Thrust
+    0x6B:"Crit while Swift St.", //Sheath Strike
     0x7E:"Damage absorption",
     0x92:"Back row multiplier",
     0x99:"Ailment chance",
+    0x9F:"Lower HP damage mult.", //Spear Reversal
+    0xAE:"[Prob. reduce turns?]",
     0xBC:"Cast preemptively", //Preemptive Taunt
     0xC3:"Amplified damage", //Charged Shot
+    0xC7:"Instant death chance", //Head Pierce
+    0xC8:"Head bind chance",
     0xD5:"Damage",
     0xD7:"Healing", //Heal Guard, Healing
     0xE1:"Minimum hits",
@@ -23,12 +33,35 @@ const subheader_obj = {
     0xF2:"Damage multiplier", //Front Guard
     0xF4:"Preemptive chance up",
     0xF6:"Field skill duration",
+    0xF9:"Party lifesteal", //Black Sabbath - all allies or only self?
+    0x122:"Self-cast after use",
     0x13A:"Damage multiplier", //Ally Shield
+    0x176:"User cur. HP % cost", //Draining Thrust
+    0x177:"Party cur. HP % cost", //Blood Fortune - how do this and Legion Thrust differ?
+    0x178:"Party cur. HP % cost", //Legion Thrust
+    0x17E:"Damage down mult.", //Shell Shock
+    0x17F:"Defense down mult.", //Shell Shock
+    0x180:"[Acc or eva mult.]", //Shell Shock
+    0x181:"[Acc or eva mult2.]", //Shell Shock
+    0x184:"[Link if del. charge?]",
+    0x18D:"Element damage mult.", //Spear Assist
     0x18F:"Static HP restore", //Healing Wall
     0x190:"Percent HP restore", //Healing Wall
+    0x191:"Static TP restore", //Allied Bonds
+    0x192:"Percent TP restore", //Allied Bonds
+    0x19B:"Musou Upper damage",
+    0x19C:"Musou Clear damage",
+    0x19D:"[Musou Swift Acc/Eva]",
+    0x19E:"[Musou Swift Acc/Eva2]",
+    0x19F:"Musou Swift spd. mult.",
+    0x1A0:"[Musou Clear ailmult.]",
+    0x1A1:"[Musou Clear ailmult2.]",
+    0x1A2:"[Musou Clear ailmult3.]",
     0x1C8:"Link on chase", //Chase Heal
     0x1C9:"Chance reduction",
+    0x1D2:"TP cost multiplier", //Act Quick
     0x1DD:"Damage multiplier", //Full Guard
+    0x1E0:"[Dmg. down per enemy]",
     0x1EB:"Can revive", //Resuscitate
     0x1EC:"Revive healing", //Resuscitate
     0x1F6:"Cooldown",
@@ -41,38 +74,75 @@ const subheader_obj = {
     0x410:"Buff damage mult.", //Shield Bash debuff
     0x411:"Buff damage mult.", //Taunt
     0x41B:"Accuracy reduction", //Trickery
+    0x41E:"Evasion reduction", //Feint Shot
     0x425:"[???] Enmity multiplier", //Taunt
+    0x432:"Ailment chance mult.", //Blood Fortune
+    0x433:"Bind chance mult.", //Blood Fortune
     0x441:"[Heal or speed mult]",
     0x446:"[Heal or speed mult2]",
     0x45B:"Healing multiplier", //Efficiency
     0x45F:"Link [Deja Vu]", //Deja Vu
+    0x473:"Damage multiplier", //ATK Up
     0x480:"Damage multiplier", //DEF UP
     0x499:"Bonus speed", //Speed Up
     0x4A5:"Chance", //Auto-Revive
     0x4A9:"Recovery chance", //Antibodies
+    0x4AA:"Link on kill",
     0x4C2:"Endure chance", //Aegis
     0x4C3:"Scavenge multiplier", //Scavenge
     0x4F9:"Health threshold",
     0x4FA:"[prob. proc chance]",
     0x505:"Overheal maximum HP", //Overheal
+    0x552:"Damage multiplier", //Duel
+    0x56F:"Damage mult. after hit", //Bloody Veil
     0x57F:"Stop blindside chance",
-    0x585:"[Eva/Acc buff]",
+    0x584:"[Maybe max activ.?]", //Royal Veil
+    0x585:"[Eva/Acc buff]", //Speed Up
     0x586:"[Eva/Acc buff2]",
+    0x5A9:"Required HP %", //Royal Veil
     0x5AA:"[Staff damage or TP]", //Staff Mastery
     0x5AD:"Chance", //En Garde
+    0x5BB:"Pierce chance", //Penetrator
+    0x5BD:"Counter link chance", //Bloodlust
+    0x5BE:"Null ailment chance",
     0x5BF:"Chance",
+    0x5C6:"[Probably chance]", //Stances
+    0x5D4:"Chance", //Multi-Shot
+    0x5E0:"Stance damage mult.", //Upper Stance
+    0x5E1:"Stance damage mult.", //Clear Stance
+    0x5E2:"[Acc or speed mult.]", //Swift Stance
+    0x5E3:"[Acc or speed mult2.]", //Swift Stance
+    0x5E4:"Speed multiplier", //Swift Stance
+    0x5E5:"[Ailment multiplier?]", //Clear Stance
+    0x5E6:"[Ailment multiplier2?]", //Clear Stance
+    0x5E7:"[Ailment multiplier3?]", //Clear Stance
+    0x5E8:"Upper damage mult.", //Full Proficiency
+    0x5E9:"Clear damage mult.", //Full Proficiency
+    0x5EA:"[Swift acc. or eva.]", //Full Proficiency
+    0x5EB:"[Swift acc. or eva2.]", //Full Proficiency
+    0x5EC:"Swift speed mult.", //Full Proficiency
+    0x5ED:"[Clear ailment mult.]", //Full Proficiency
+    0x5EE:"[Clear ailment mult2.]", //Full Proficiency
+    0x5EF:"[Clear ailment mult3.]", //Full Proficiency
+    0x5F8:"Act last", //Spear Assist
     0x633:"Force boost duration",
     0x638:"Shield protect boost",
     0x63A:"Intensive Care TP mult.",
-    0x63B:"ICare [speed or mult]",
+    0x63B:"ICare [speed or mult]", //Intensive Care
     0x63C:"ICare [speed or mult2]",
+    0x640:"Damage multiplier", //Hero Battle
+    0x645:"Second use dmg. mult.", //Double Action
+    0x64F:"Party lifesteal", //Hero Battle
     0x665:"Evasion multiplier", //Illusory Formation
     0x668:"Speed multiplier", //Illusory Formation
+    0x675:"Maximum effect level",
     0x676:"[Break eff. stage 1]",
     0x677:"[Break eff. stage 2]",
     0x678:"[Break eff. stage 3]",
+    0x682:"Threshold ail. chance",
     0x6B7:"HP multiplier", //HP Up
     0x6B6:"[Probably TP mult.]",
+    0x6B9:"TP multiplier", //TP Up
 }
 
 const ENEMY_SKILL_NAME = ["None","Attack","Jump Kick","Harsh Buzz","Rising Bile","Thunder Strike","Paralysis Roll","Shell Guard","Swing","Muddle Roar","Stomp","Sleep Breath","Poison Breath","Charge","Tail Swipe","Coconut Strike","Tangling Vine","Piercing Stinger","Rising Fang","Drain","Sweet Mist","Corrode","Fireball","Rush","Slumber Needle","Ruinous Strike","Double Slash","Sharp Fang","Jumping","Thorn","Bite Off","Zone Icicle","Pincer Shield","Icy Leg Lash","Diving Press","Crush","Poison Sting","Frigid Tail Whip","Absolute Fight","Absolute Fight","Absolute Fight","Sealing Volt","Black Wings","Petrifying Stare","Talon Zapper","Wind Flash","Air Slash","Rage Fangs","Voltking's Fury","Raging Winds","Rending Claw","Horn Strike","Foul Spew","Binding Claw","Cover","Shield Shell","Ruin Fangs","Chomp","Slumber Claw","Mangle Fang","Ice Slurry","Ancient Curse","Ancient Curse","Swim Frenzy","Calming Wave","Mow Down","Feeding Vortex","Terrible Roar","Baboon Dunk","Tackle","Leg Sweep","Toxic Mist","Ground Dash","Fire Breath","Crimson Gale","Balloon Drop","Dust Wings","Guard Wing","Flame Howl","Sleep Pollen","Stampede","Burning Wind","Mucus Frenzy","Unload","Devour Ally","Devour Ally","Devour Ally","Raging Flame","Hound Howl","Potent Acid","Electrified Juice","Pineapple Mash","Scarlet Glare","Ground Shake","Panic Dive","Flame Spell","Noxious Fang","Flame Burst","Carapace Wall","Crazy Swipe","Recoil Needle","Peck Rush","Poison Dust","Vertigo Gel","Solar Flame","Giant Swing","Snail Dance","Bind Fang","Echolocation","Bloodsucker","Ferocious Fish","Paralyze Fang","Menacing Glare","Assault Wing","Frigid Wind","Predatory Ice","Stance","Counter Blow","Counter Blow","Techno Bash","Techno Bash","Power Tackle","Blinding Spores","Manic Ambush","Giant Hammer","Holy Shock","Trample","Scalding Juice","Panic Assault","Thunderclap","Pandemistrike","Carnage Claw","Magnetic Storm","Magnetic Storm","Dozing Gaze","Bind Mace","Violent Beating","Crushing Kick","Poison Spores","Lava Boulder","Icicle Lump","Hama Dive","Stupefying Buzz","Blazing Lance","Snowy Claw","Mighty Pierce","Electric Dive","Oil Spin","Glider","Shell Cutter","Stunning Glare","Scorching Glare","Onrush","Calamity Roar","Red-Hot Fangs","Blade Fin","Violent Crash","Crushing Kick","Volt Shot","Forest Breach","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","Cross Counter","Evil Cry","Seal Arms","Stone Gleam","Leg Slash","Firestorm","Silent Claw","Frigid Branch","None","Siren Song","Wide Bind","Leg Hex","Supreme Bind","High Ecstasy","Death Glare","Volt Strike","Reverse Solids","Volt Shoulder","Feral Ice Fang","Knockout Tackle","Multi-Stab","Illusion Kick","Blast Horn","Arm Slash","Purge","King's Claws","Slumber Mist","Ice Shot","Limb Crusher","Spirit Eroder","Hammerhead","Toxic Comet","Riposte Fang","Guillotine Chomp","Ivy Drain","Binding Leaf","Licking","Freezing Jump","Dive Twister","Thrust Up","Cursed Kick","Heavenly Bolt","Cold Front","Jolting Blow","Ruinous Pollen","Flame Claw","Plague Club","Leg Zapper","Scorching Juice","Scythe Slash","Slash Rampage","Tri-Death","Charge","Ruinous Strike","Arm Breaker","Rock Crusher","None","Stunning Roar","Berserker Rage","Berserk","Silent Stare","Cross Counter","Fire Rush","Combo Smash","Hurricane Punch","None","Cure","Holy Veil","Mud Throw","Fin Strike","Whip Ripper","Marsh Dive","Earthquake","Rest","Ram","Wing Talon","Tail Strike","Whirlwind","Galvanic Ray","Shining Breath","Shrill Cry","Chaos Scream","Claws of Despair","Storm Feather","Feast","Bloodbath","Grand Berry","Freeze Ripple","Great Melody","Ocean Rave","Spout","Ancient Roll","Ancient Roll","Thunderwing","Pinion Spear","Deathbringer","Chaosbringer","Grip","Wind Breath","Menacing Gust","Vine Grab: Head","Vine Grab: Arm","Vine Grab: Leg","Suspicious Pollen","Darkness Rend","Poison Mist","Deathly Glare","Thousand Hertz","Sinister Flame","Dark Mist","Avarice Claw","Crushing Tail","Hellfire Breath","Healing Howl","Selfless Love","Fire Breath","Harsh Cry","Dust Tail","Scorching Fire","Fire Cannon","Horned Hurl","Sleep Tail","Fire Wall","Fire Wall","Fire Regen","Fire Chaser","Scale Stuck","Scale Summon","Snake Pile","Great Blaze","Double Strike","Menacing Roar","Doom Assault","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","Doom Breath","Bite","Imperial Claws","Pound","Call Allies","Roar","Stone Gleam","Pierce","Thrust","Safeguard","Thunder Storm","Hail Storm","Cursed Gas","Petrifying Gas","Panic Gas","Self Heal","Self Heal Active","Self Heal Inactive","Resonant Growl","Shadow Remnant","Wide Bravery","Guard Rush EX","Frigid Slash EX","Act Breaker","Ominous Wave","Clear Mind EX","Shadow Burst","Miracle Edge EX","Binding Light","Sealing Flame","Quaking Thunder","Clean Sweep","Power Press","Hateful Glare","Megalodive","Mode Change","Mode Change","Pentacle Slam","Pentacle Slam","Call Front","Icicle Jail","Icicle Jail","Lick","Ice Storm","Poison Spit","Slurp","Reflection","Rock Hammer","Sledgehammer","Regen","Reflection","Endure","Flame Sigh","Frost Smile","Lightning","Tendril Thrash","Vampiric Kiss","Ancient Pollen","Spider Lily","Lightning Storm","Back Kick","Clawbinder","Panic Glare","Ebon Gaze","Sky Dive","Rest","Wing Shield","Wing Shield Down","Cry Soul","Lullaby","Piercing Shell","Ice Shave","Six Sins","Summon Tentacle","Shocking Bind","Ghost Tentacle","Squeeze Grip","Tail Whip","Serpent's Curse","Serpent's Curse","Poison Flame","Evil Eye","Death's Embrace","Alluring Body","Familiar","Head Bind","Arm Bind","Leg Bind","Binding Thrash","None","None","Copycat","Gorge","Throw","Fuse","Gorge","None","Summon","None","None","Stunning Juice","None","None","None","None","None","None","None","None","Glinting Blade","None","None","None","Reversal Slam","None","None","None","None","None","None","None","Bad Beak","None","None","None","Dream Eater","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","Boulder Crush","None","Putrid Breath","Violent Cry","Berserk Cry","Reckless Barrier","Tusk Strike","Brandish","Eradicate","King's Rush","Grind","Queen's Verdict","Duster","Ruler's Eye","Empress Flare","Guard Line","Healing Honey","Repressive Howl","Cold Fangs","Howl","Fierce Rush","Silver Fang","Stunning Howl","Howl","Fire Torrent","Echoing Howl","Dragon Smash","Draconic Fury","Dragon Claw","Red Fang","Summon Ally","Resonant Fire","Ice Breath","Iceblock","Crushing Arm","Glacial Regen","Sonic Scream","Triple Strike","Summon Ally","Resonant Ice","Glacial Regen","Volt Breath","Soul Strike","Judgement","Corrupt Howl","Binding Dance","Summon Ally","Resonant Thunder","Resonant Thunder","Resonant Thunder","Scorching Hell","Frozen Hell","Thunderous Hell","Dark Lunge","Hellish Stab","Endless Drop","Dark Invitation","Lure of Death","None","None","None","None","None","None","None","Dream Eater","None","Glinting Blade","Stunning Juice","None","None","None","None","None","None","Combo Stance","Reversal Slam","None","None","Unholy Light","Unholy Light","Violent Blades","Undead Regen","Rearing Head","Death Tumble","Condemnation","Eternal Body","Eternal Body","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","Change Spell","Bracing Liquid","Bracing Liquid","Binding Juice","Binding Juice","Camouflage","Icicle","Rhino Rampage","Muddy Plague","Headbutt Binder","Pandemic March","Jet Uppercut","Boomerang Hook","Back Smash","Choking Vine","Wind Flash EX","Techno Bash","Blizzard","Frozen Tongue","Entrapment","Entrapment","Binding Root","Ripping Arm","Legsnap Needle","Deadly Vortex","Sudden Slash","Vertigo Lash","Confusing Dust","Slice and Dice","Sticky Spit","Toxic Spew","Mind Ruin","Cruel Fangs","Snow Cover","Dynamo Thunder","Dust Storm","Destructive Fang","Roar of Triumph","Devourer Needle","Molt","Needle Shower","Binding Shine","Conviction Shine","Coiling Vine","Infernal Flame","Attack Line EX","Leg Bite","Infernal Blaze","Earthquake","Lonely Fang","Scissor Wrath","Scissor Wrath","Scissor Wrath","Refreshing Honey","Refreshing Honey","Ice-Cold Acid","Piercing Beak","Heavy Claw","Claw Slice","Ominous Wind","Self-Destruct","Insanity Scales","Rock Coil","Spark","Biting Flurry","Freezing Juice","Pinebuster","Wintry Moon","Heat Wave","Reed Cutter","Dead Zone","Eternal Chant","Absorb Vine","Puke Shower","Strike Barrage","Furious Glare","Piercing Gaze","Severing Claw","Enchanted Eyes","Crazy Eye","Focused Bite","Demonic Shine","Horned Hurl","Deer Outrage","Frigid Scythe","Frozen Fury","Blind Blade","Curse Fang","Sonic Fang","Playful Bite","Frolic","Pout","Eruption","Death Glare","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None","None"]; //it's gross but there's really no reason to load it as a data file tbh
